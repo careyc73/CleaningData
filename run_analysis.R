7bd79bf04b92b6b75a89075f342ef30ef4fc741f
Chris Carey <careyc@chicagotrading.com>
2014-05-10 23:35:08 -0500

    Only grab mean and std columns

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,27 @@
 library("data.table")
 
-loadDataSet <- function(type) {
+getFeatureColumnsOfType <- function (featuresTable, disjunctiveFilters) {
+    filterVector <- vector(mode="logical", length=nrow(featuresTable))
+    for (filter in disjunctiveFilters) {
+        filterVector <- grepl(filter, featuresTable[[2]]) | filterVector
+    }
+
+    filterVector
+}
+
+loadDataSet <- function(type, desiredColumns) {
     dataSet <- data.table(read.table(paste("X_", type, ".txt", sep="")))
+    
+    dataSet <- dataSet[,desiredColumns,with=FALSE]
     
     dataSet[,subject := data.table(read.table(paste("subject_", type, ".txt", sep="")))]
     dataSet[,activity := data.table(read.table(paste("y_", type, ".txt", sep="")))]
 }
 
-combinedData <- loadDataSet("test")
-combinedData <- rbind(combinedData, loadDataSet("train"))
+featuresTable <- read.table("features.txt")
+
+# Assumption:  mean is ONLY mean, not meanFreq
+desiredColumns <- getFeatureColumnsOfType(featuresTable, c("mean()", "std()"))
+
+combinedData <- loadDataSet("test", desiredColumns)
+combinedData <- rbind(combinedData, loadDataSet("train", desiredColumns))
