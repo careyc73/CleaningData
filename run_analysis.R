320ce194377c979c1de5e118ed9f8bffbc0f8fd6
Chris Carey <careyc@chicagotrading.com>
2014-05-11 22:24:09 -0500

    Substitute in the textual activity description, get rid of the numerical code

--- run_analysis.R
+++ run_analysis.R
@@ -18,9 +18,17 @@ loadDataSet <- function(type, desiredColumns) {
     dataSet[,activity := data.table(read.table(paste("y_", type, ".txt", sep="")))]
 }
 
+getActivityText <- function(activityTable, numericActivity) {
+    activityTable[numericActivity,2]
+}
+
 # Read the features information in, this file contains a reasonable label for each
 # column in the dataset.
 featuresTable <- read.table("features.txt", stringsAsFactors=F)
+
+# The activity_labels file contains the textual representations of the activities which
+# were part of the study.
+activityTable <- read.table("activity_labels.txt", stringsAsFactors=F)
 
 # Assumption:  mean is ONLY mean, not meanFreq.  Use the features file to determine
 # which columns we care about -- specifically mean and std in this implementation but note
@@ -35,3 +43,7 @@ combinedData <- rbind(combinedData, loadDataSet("train", desiredColumns))
 # file, note the activity column which was added from the y_(test|train).txt file is left
 # unchanged.
 setnames(combinedData, 1:66, featuresTable[desiredColumns,][[2]])
+
+# Take the numeric activity code in the activity column and replace it with the more
+# human-friendly textual code which the activity_labels file contains.
+combinedData[,activity := getActivityText(activityTable,activity)]
