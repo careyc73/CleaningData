fabffcd170a35fd003da5691b55bd4f26f8fea38
Chris Carey <careyc@chicagotrading.com>
2014-05-11 22:05:23 -0500

    Set column names to match what's in the features file

--- run_analysis.R
+++ run_analysis.R
@@ -18,10 +18,20 @@ loadDataSet <- function(type, desiredColumns) {
     dataSet[,activity := data.table(read.table(paste("y_", type, ".txt", sep="")))]
 }
 
-featuresTable <- read.table("features.txt")
+# Read the features information in, this file contains a reasonable label for each
+# column in the dataset.
+featuresTable <- read.table("features.txt", stringsAsFactors=F)
 
-# Assumption:  mean is ONLY mean, not meanFreq
-desiredColumns <- getFeatureColumnsOfType(featuresTable, c("mean()", "std()"))
+# Assumption:  mean is ONLY mean, not meanFreq.  Use the features file to determine
+# which columns we care about -- specifically mean and std in this implementation but note
+# that the getFetaureColumnsOfType will accept any set of patterns and or them.
+desiredColumns <- getFeatureColumnsOfType(featuresTable, c("mean\\(", "std\\("))
 
+# Load the test data set and then add the train data set to it...
 combinedData <- loadDataSet("test", desiredColumns)
 combinedData <- rbind(combinedData, loadDataSet("train", desiredColumns))
+
+# Set the column names in the data table to the column names indicated by the features
+# file, note the activity column which was added from the y_(test|train).txt file is left
+# unchanged.
+setnames(combinedData, 1:66, featuresTable[desiredColumns,][[2]])
