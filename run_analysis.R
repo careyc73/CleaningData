e2cf47377a30d237c4b5905539c71da07261590b
Chris Carey <careyc@chicagotrading.com>
2014-05-15 22:32:02 -0500

    Means now working plus a lot of commenting / structure

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,29 @@
+#Load the data.table library, the data will be loaded in a data table
 library("data.table")
 
-getFeatureColumnsOfType <- function (featuresTable, disjunctiveFilters) {
-    filterVector <- vector(mode="logical", length=nrow(featuresTable))
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# Script Functions
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+
+# Takes a list of strings and a vector of text patterns.  Iterate through the text patterns
+# and identifies which strings in the list contain any of the text patterns as a substring.
+# Returns a logical vector identifying the matching strings.
+getFeatureColumnsOfType <- function (columnLabelsTable, disjunctiveFilters) {
+    logicalFilterVector <- vector(mode="logical", length=nrow(columnLabelsTable))
     for (filter in disjunctiveFilters) {
-        filterVector <- grepl(filter, featuresTable[[2]]) | filterVector
+        logicalFilterVector <- grepl(filter, columnLabelsTable[[2]]) | logicalFilterVector
     }
 
-    filterVector
+    logicalFilterVector
 }
 
+# Takes a parameter indicating which 'type' of data is being loaded -- really an append
+# on file names indicating test or training data, and returns a data.table retrieved from
+# the file the type resolves to.  The table will be pared to include only the columns
+# indicated by the desiredColumns vector.  Additionally the activity and subject for a given
+# row will be bound into the table from the subject_ and y_ files.
 loadDataSet <- function(type, desiredColumns) {
     dataSet <- data.table(read.table(paste("X_", type, ".txt", sep="")))
     
@@ -18,41 +33,59 @@ loadDataSet <- function(type, desiredColumns) {
     dataSet[,activity := data.table(read.table(paste("y_", type, ".txt", sep="")))]
 }
 
-getActivityText <- function(activityTable, numericActivity) {
-    activityTable[numericActivity,2]
+# Shortcut function to retrieve the textual activity associated with a numerical activity
+# code passed in.
+getActivityText <- function(activityLabelsTable, numericActivity) {
+    activityLabelsTable[numericActivity,2]
 }
 
-# Read the features information in, this file contains a reasonable label for each
+# Take a factor, a data table, and an accumulation function.  Split the data table by the factor
+# and then apply the accumulation function to the grouped results.  Return the outcome of the
+# accumulation function + sapply
+getFactoredAccumulation <- function(factor, dataTable, accumulationFuction) {
+    sapply(split(dataTable, factor),accumulationFuction)
+}
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# Script workflow
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+
+# Read the features.txt file in, this file contains a reasonable label for each
 # column in the dataset.
-featuresTable <- read.table("features.txt", stringsAsFactors=F)
+columnLabelsTable <- read.table("features.txt", stringsAsFactors=F)
 
 # The activity_labels file contains the textual representations of the activities which
 # were part of the study.
-activityTable <- read.table("activity_labels.txt", stringsAsFactors=F)
+activityLabelsTable <- read.table("activity_labels.txt", stringsAsFactors=F)
 
-# Assumption:  mean is ONLY mean, not meanFreq.  Use the features file to determine
+# Assumption:  mean is ONLY mean, not meanFreq.  Use the column labels to determine
 # which columns we care about -- specifically mean and std in this implementation but note
-# that the getFetaureColumnsOfType will accept any set of patterns and or them.
-desiredColumns <- getFeatureColumnsOfType(featuresTable, c("mean\\(", "std\\("))
+# that the getFetaureColumnsOfType will accept any set of patterns and logically 'or' them.
+desiredColumns <- getFeatureColumnsOfType(columnLabelsTable, c("mean\\(", "std\\("))
 
-# Load the test data set and then add the train data set to it...
-combinedData <- loadDataSet("test", desiredColumns)
-#ToDo:  I should be doing this via a key join
-combinedData <- rbind(combinedData, loadDataSet("train", desiredColumns))
+# Load the test data set
+testingAndTrainingDataTable <- loadDataSet("test", desiredColumns)
+# Load the train data set as well and append it to the data table.
+testingAndTrainingDataTable <- rbind(testingAndTrainingDataTable, loadDataSet("train", desiredColumns))
 
 # Set the column names in the data table to the column names indicated by the features
-# file, note the activity column which was added from the y_(test|train).txt file is left
+# file, the activity and subject columns which were added from the y_(test|train).txt file are left
 # unchanged.
 # The combined gsub calls are stripping the non-alphanumerical characters ("-()") from the column
 # names and capitalizing "mean" and "std" to make for more readable column names.
-setnames(combinedData, 1:66, 
-         gsub("std", "Std", gsub("mean", "Mean", gsub("[-(\\()]", "", featuresTable[desiredColumns,][[2]]))))
+# I PREFER TO LEAVE MOST OF THE ORIGINAL CAPITALIZATION IN PLACE.  Capitalization makes varibles that 
+# have a compound name read more naturally.
+readableNames <- gsub("std", "Std", gsub("mean", "Mean", gsub("[-(\\()]", "", columnLabelsTable[desiredColumns,][[2]])))
+setnames(testingAndTrainingDataTable, 1:66, readableNames)
 
 # Take the numeric activity code in the activity column and replace it with the more
 # human-friendly textual code which the activity_labels file contains.
-combinedData[,activity := getActivityText(activityTable,activity)]
+testingAndTrainingDataTable[,activity := getActivityText(activityLabelsTable,activity)]
 
-# ToDo
-# Just a first cut at extracting out the summary data, this gets me the mean of tBodyAccMeanX
-# but still need to figure out getting the mean of all columns.
-tidySummary <- combinedData[,mean(tBodyAccMeanX),by=list(activity,subject)]
+# Build a factor that groups all rows by the combination of (subject & activity)
+factorToSplitData <- list(factor(testingAndTrainingDataTable$activity), factor(testingAndTrainingDataTable$subject))
+    
+# Calculate the mean for each {subject, activity} pair and assign the result to "meansInTidyForm".
+meansInTidyForm <- getFactoredAccumulation(factorToSplitData, testingAndTrainingDataTable, mean)
